288eaa6f1eaa872d03dc809088bf47aa8a04b12a
Artur Podsiadły <artpods56@gmail.com>
2025-10-29 13:36:31 +0100

    chore: update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,34 @@
+import com.opencsv.CSVParser;
+import com.opencsv.CSVReader;
 import cost.MSE;
 
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+
+        final String COMMA_DELIMITER = ",";
+
+        CSVParser
+
+        List<List<String>> records = new ArrayList<List<String>>();
+        try (CSVReader csvReader = new CSVReader(new FileReader("book.csv"))) {
+            String[] values = null;
+            while ((values = csvReader.readNext()) != null) {
+                records.add(Arrays.asList(values));
+            }
+        }
+
+
         MSE mse = new MSE();
 
 
-        ArrayList<Double> arr1 = new ArrayList<Double>(Arrays.asList(2.0,3.0,4.1));
+        ArrayList<Double> arr1 = new ArrayList<Double>(Arrays.asList(2.0, 3.0, 4.1));
 
-        ArrayList<Double> arr2 = new ArrayList<Double>(Arrays.asList(1.0,7.1,3.14));
+        ArrayList<Double> arr2 = new ArrayList<Double>(Arrays.asList(1.0, 7.1, 3.14));
 
 
         double cost = mse.computeCost(arr1, arr2);
--- src/main/java/models/LinearRegression.java
+++ src/main/java/models/LinearRegression.java
@@ -1,5 +1,6 @@
 package models;
 
+import cost.MSE;
 import utils.ArrayValidator;
 
 import java.util.ArrayList;
@@ -8,14 +9,76 @@ public class LinearRegression {
     private double slope;
     private double intercept;
     private boolean isFitted;
+    private double learningRate;
+    private int epochs;
+    private MSE mse;
 
+
+    public LinearRegression(double learningRate, int epochs) throws Exception{
+        this.validateModelParams(learningRate, epochs);
+        this.slope = 1;
+        this.intercept = 1;
+        this.isFitted = false;
+        this.learningRate = learningRate;
+        this.epochs = epochs;
+        this.mse = new MSE();
+    }
+
+    private void validateModelParams(double learningRate, int epochs) throws Exception{
+        if (learningRate == 0) {
+            throw new Exception("The model can't be initialized with learning rate set to 0.0");
+        }
+
+        if (epochs == 0) {
+            throw new Exception("The model won't run of the number of epochs is set to 0");
+        }
+    }
 
     private double compute(double x){
         return (this.slope * x) + this.intercept;
     }
 
-    public void fit(ArrayList<Double> x, ArrayList<Double> y) throws Exception {
-        ArrayValidator.validateInputs(y,x);
+    private double computeSlopeDerivative(ArrayList<Double> xArray, ArrayList<Double> yTrue, ArrayList<Double> yPred) {
+
+        double derivativeSum = 0;
+
+        for (int i = 0; i < yTrue.size(); i++) {
+            derivativeSum += xArray.get(i)*(yTrue.get(i) - yPred.get(i));
+        }
+
+        return (-2 * derivativeSum) / xArray.size();
+    }
+
+    private double computeInterceptDerivative(ArrayList<Double> yTrue, ArrayList<Double> yPred) {
+
+        double derivativeSum = 0;
+
+        for (int i = 0; i < yTrue.size(); i++) {
+            derivativeSum += (yTrue.get(i) - yPred.get(i));
+        }
+
+        return (-2 * derivativeSum) / yTrue.size();
+    }
+
+    public void fit(ArrayList<Double> xArray, ArrayList<Double> yTrue) throws Exception {
+        ArrayValidator.validateInputs(yTrue,xArray);
+
+        for (int i = 0; i < this.epochs; i++) {
+            ArrayList<Double> yPred = this.predict(xArray);
+
+            this.slope = this.slope - (this.learningRate * this.computeSlopeDerivative(xArray, yTrue, yPred));
+            this.intercept = this.intercept - (this.learningRate * this.computeInterceptDerivative(yTrue, yPred));
+
+            if (i % 10 == 0) {
+                double mse = this.mse.computeCost(yTrue, yPred);
+                System.out.printf("Epoch: " + i + ", Loss: " + mse);
+            }
+
+        }
+
+        this.isFitted = true;
+
+
 
     }
 
